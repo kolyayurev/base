8425878484793be373c2143761bee624ea27e793
kolyayurew <kolyayurew@gmail.com>
2021-10-08 19:07:23 +0300

    save

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\MetaTagsServiceProvider::class,
+        App\Voyager\Providers\VoyagerServiceProvider::class,
 
     ],
 
